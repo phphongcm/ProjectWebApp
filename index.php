ee637fe4674eb8e6d05e58bd4b50cbe7249e08cd
Phong Phạm <phphongbinhdinh@gmail.com>
2019-05-12 15:00:40 +0700

    est

--- index.php
+++ index.php
@@ -4,10 +4,32 @@
 <meta charset="utf-8">
 <title>Untitled Document</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link type="text/css" href="css/css.css">
-</head>
-
-<body>
+<link rel="stylesheet" href="css/css.css">
+<style>
 	
+</style>
+</head>
+	
+<body>
+<div id="wrapper">
+	<div id="menu-top">
+		<div id="menu-top-left"> menu trai </div>
+			<div id="menu-top-left-menu">
+				<ul>
+				<li> 24h qua </li>
+				<li> RSS </li>
+				</ul>
+			</div>
+		<div id="menu-top-right"> menu phai </div>
+		
+	</div>
+	<div id="banner"> banner </div>
+	<div id="navigator"> Menu Navi </div>
+	<div id="content"> content 
+		<div id="content-left"> noidung trai </div>
+		<div id="conntent-right"> coi dung phai </div>	
+	</div>
+	<div id="footer"> footer </div>
+</div>
 </body>
 </html>
